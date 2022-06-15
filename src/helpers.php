852985f818ca658f57b5a2fe0db2bf3c3f960023
Shashwat Mishra <11258035+secrethash@users.noreply.github.com>
2022-06-15 22:08:04 +0530

    Fixed: Settings Helper

--- src/helpers.php
+++ src/helpers.php
@@ -5,15 +5,13 @@ use Vidwan\Settings\Models\Setting;
 if (! function_exists('settings')) {
     function settings($key = null, $default = null)
     {
-        if (tenant()) {
-            if ($key) {
-                $setting = Setting::where('key', $key)->first();
+        if ($key) {
+            $setting = Setting::where('key', $key)->first();
 
-                return $setting ? $setting->value : $default;
-            }
-
-            return Setting::all();
+            return $setting ? $setting->value : $default;
         }
+
+        return Setting::all();
 
         return $default;
     }
